57231de572b406eb7c1130d7dc23fb7bd9ee2428
Roman Lyubimov <raman@cmstuning.net>
2018-04-11 12:23:16 +0300

    export user credentials

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,4 @@ export * from './authenticators/oauth2-password-grant';
 export * from './authorizers/oauth2-bearer';
 export * from './session-stores/local-storage';
 export * from './session.service';
+export * from './user-credentials';
